b265c768a28f22e86a0d2570b8e97b68b646320f
chiarasch <chiara_schiller@t-online.de>
2026-03-09 09:46:22 +0100

    bump version

--- src/cozipy/__init__.py
+++ src/cozipy/__init__.py
@@ -1,5 +1,5 @@
 from cozipy.nep_cozi import nep_analysis, run_cozi
 from cozipy.neighbors import knn_graph, radius_graph, delaunay_graph
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["nep_analysis", "run_cozi", "knn_graph", "radius_graph", "delaunay_graph"]
